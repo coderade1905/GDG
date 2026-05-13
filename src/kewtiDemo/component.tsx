68bdbbd9b2b41b5a7c429fb662bf1441bfe6d372
Kaleab Belayhun <kaleabbelayhun01@gmail.com>
2026-05-13 17:09:28 +0300

    added kewtiInput

--- src/kewtiDemo/component.tsx
+++ src/kewtiDemo/component.tsx
@@ -1,8 +1,11 @@
+import {KewtiInput} from "../kewti-inputs/component";
+
 export default function KewtiDemo() {
     return (
         <div className="h-full w-full flex items-center justify-center">
             <div className="mt-30">
                 <h1>A simple page for testing components</h1>
+                <KewtiInput variant="input" />
             </div>
         </div>
     );
